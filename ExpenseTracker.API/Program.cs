3c22db2f8cf7a44928130a9c03b22d3032eb919e
RohithR24 <srohithreddy.edu@gmail.com>
2024-08-16 23:40:50 -0700

    Adding the setup code

---
+++ ExpenseTracker.API/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.MapGet("/", () => "This is ExpenseTracker 1.0");
+
+app.Run();
