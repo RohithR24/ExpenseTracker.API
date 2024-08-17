dee8226ffac5b30ba7ba087a42ae7b8be473e6dd
RohithR24 <srohithreddy.edu@gmail.com>
2024-08-17 12:13:20 -0700

    Modifying the data models

--- ExpenseTracker.API/Data/Models/Category.cs
+++ ExpenseTracker.API/Data/Models/Category.cs
@@ -6,5 +6,6 @@ namespace Data.Models
         public int UserId { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
+        public User User { get; set; }
     }
 }
