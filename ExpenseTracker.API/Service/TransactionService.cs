798cf2b7d59812be3e613d9e1e1bc0e651a0e290
RohithR24 <srohithreddy.edu@gmail.com>
2024-09-02 23:50:42 -0700

    Adding the constraint

--- ExpenseTracker.API/Service/TransactionService.cs
+++ ExpenseTracker.API/Service/TransactionService.cs
@@ -4,47 +4,61 @@ using Mappings;
 using Repository;
 using Service.Impl;
 
-namespace Service{
+namespace Service
+{
 
-    public class TransactionService: ITransactionService{
+    public class TransactionService : ITransactionService
+    {
 
-            private readonly ITransactionRepository _transactionRepository;
-            private readonly ITransactionMapping _transactionMapping;
-            private readonly IBudgetRepository _budgetRepository;
+        private readonly ITransactionRepository _transactionRepository;
+        private readonly ITransactionMapping _transactionMapping;
+        private readonly IBudgetRepository _budgetRepository;
 
-        public TransactionService(ITransactionRepository transactionRepository, ITransactionMapping transactionMapping, IBudgetRepository budgetRepository){
+        public TransactionService(ITransactionRepository transactionRepository, ITransactionMapping transactionMapping, IBudgetRepository budgetRepository)
+        {
             _transactionRepository = transactionRepository;
             _transactionMapping = transactionMapping;
             _budgetRepository = budgetRepository;
         }
-        public bool AddTransaction(NewTransaction createTransaction, out string Message){
+        public bool AddTransaction(NewTransaction createTransaction, out string Message)
+        {
 
-                var transaction =  _transactionMapping.ToTransactionEntity(createTransaction);
-                if(BudgetCheck(transaction)){
+            var transaction = _transactionMapping.ToTransactionEntity(createTransaction);
+
+            if (transaction.Type == "Income")
+            {
+                bool result = _transactionRepository.AddTransaction(transaction);
+                Message = "Ok";
+                return result;
+            }
+            else
+            {
+                if (BudgetCheck(transaction))
+                {
                     bool result = _transactionRepository.AddTransaction(transaction);
-                    Message= "Ok";
+                    Message = "Ok";
                     return result;
                 }
-                else{
-                    Message= "Out of Budget";
+                else
+                {
+                    Message = "Out of Budget";
                     return false;
                 }
 
-
-               
-
-                return false;
-        }
-
-        private bool BudgetCheck(Transaction transaction){
-
-            var budget  = _budgetRepository.FetchBudget(transaction.UserId, transaction.CategoryId, transaction.Date);
-
-            if(budget.Amount >= transaction.Amount){
-                _budgetRepository.UpdateBudgetAmount(budget.UserId, budget.Amount-transaction.Amount);
-                return true;
             }
 
+        }
+
+        private bool BudgetCheck(Transaction transaction)
+        {
+
+            var budget = _budgetRepository.FetchBudget(transaction.UserId, transaction.CategoryId, transaction.Date);
+
+            if (budget.Amount >= transaction.Amount)
+            {
+                _budgetRepository.UpdateBudgetAmount(budget.UserId, budget.Amount - transaction.Amount);
+                return true;
+            }
             return false;
         }
     }
